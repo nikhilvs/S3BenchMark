991f0b277cf3c0c9b614b799912d98ed47d1abaa
nikhilvs9999 <nikhil.vs@hubblehome.com>
2016-02-17 22:21:27 +0530

    working commit

--- src/main/java/com/hubbleconnected/monitor/s3monitor/S3BenchMark.java
+++ src/main/java/com/hubbleconnected/monitor/s3monitor/S3BenchMark.java
@@ -8,30 +8,34 @@ import java.util.concurrent.ThreadFactory;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectResult;
 import com.amazonaws.services.s3.transfer.TransferManager;
 import com.amazonaws.services.s3.transfer.Upload;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
-import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 
 public class S3BenchMark {
 
     private static final Logger log = Logger.getLogger(S3BenchMark.class);
-    private static ExecutorService s3CheckThreadPool;
+    private static final ExecutorService s3CheckThreadPool;
     private static final int S3_CHECK_THREAD_POOL = 1000;
-    private static ExecutorService s3TransferManagerThreadPool;
+    private static final ExecutorService s3TransferManagerThreadPool;
     private static final int S3_TRANSFER_MANAGER_THREAD_POOL = 1000;
     private static AmazonS3 s3;
     private static TransferManager tx;
 
     private static final String BUCKET = "dev-h2o-upload-server";
-        private static final String CLIP_PATH="/home/ec2-user/clip.flv";
+    private static final String CLIP_PATH = "/home/ec2-user/clip.flv";
 //    private static final String CLIP_PATH = "/Users/nikhilvs9999/Documents/poc.java";
     ///Users/nikhilvs9999/Documents/poc.java
 
@@ -53,23 +57,24 @@ public class S3BenchMark {
         tx = new TransferManager(s3, s3TransferManagerThreadPool, true);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-//        String pool=System.getProperty("pool");
-//        if(pool!=null && !pool.isEmpty())
-//        {
-//            
-//        }
         final Path filePath = Paths.get(CLIP_PATH);
         final File uploadFile = filePath.toFile();
+        InputStream in = new FileInputStream(uploadFile);
         Callable<Long> c = () -> {
             long t1 = System.currentTimeMillis();
-                    Upload upload = tx.upload(BUCKET, UUID.randomUUID().toString(), uploadFile);
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentLength(uploadFile.length());
+
+            Upload upload = tx.upload(BUCKET, UUID.randomUUID().toString(), in, objectMetadata);
+            PutObjectResult result = s3.putObject(BUCKET, UUID.randomUUID().toString(), uploadFile);
 //            Upload upload = tx.upload(BUCKET, "hello/mello" + System.currentTimeMillis(), uploadFile);
-            upload.waitForCompletion();
+//            upload.waitForCompletion();
             long t2 = System.currentTimeMillis();
             long time = t2 - t1;
-            log.info("upload time :" + time + " status :" + upload.isDone());
+//            log.info("upload time :" + time + " status :" + upload.isDone());
+            log.info("upload time :" + time + " status :" + result.getETag());
             return time;
         };
 
@@ -88,13 +93,13 @@ public class S3BenchMark {
                 } catch (InterruptedException | ExecutionException e) {
                     throw new IllegalStateException(e);
                 }
-            }).forEach(System.out::println);
+            });
         } catch (InterruptedException ex) {
             log.error(ex);
         }
         long t2 = System.currentTimeMillis();
         long time = t2 - t1;
-        log.info("Total time taken :" + time + " so req/sec :" + (S3_CHECK_THREAD_POOL*1000 / time));
+        log.info("Total time taken :" + time + " so req/sec :" + (S3_CHECK_THREAD_POOL * 1000 / time));
         s3CheckThreadPool.shutdown();
 //        tx.shutd;
 
